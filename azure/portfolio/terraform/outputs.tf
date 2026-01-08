2a10df6eaaaf30869198e5b4a9a63292ce11c327
Morten Victor Nordbye <morten@nordbye.it>
2026-01-08 19:39:07 +0100

    feat/removed-old

--- azure/portfolio/terraform/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "resource_group" { value = azurerm_resource_group.rg.name }
-output "aca_env" { value = azurerm_container_app_environment.env.name }
-output "aca_app" { value = azurerm_container_app.app.name }
--- k8s/k3sa/README.md
+++
@@ -1,4 +0,0 @@
-First we need to install crds
-kubectl apply -k https://github.com/argoproj/argo-cd/manifests/crds\?ref\=stable
-Then we need to install argocd
-kubectl apply -k sets
